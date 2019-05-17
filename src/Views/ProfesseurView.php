374a89b00ea43bf25e2f85690fe7e4d06479c75c
thomascormier <thomas.cormier.00@gmail.com>
2019-05-17 17:23:36 +0200

    ProfesseurView

--- src/Views/ProfesseurView.php
+++ src/Views/ProfesseurView.php
@@ -12,17 +12,43 @@ class ProfesseurView{
                 <head>
                     <meta charset="utf-8">
                     <title>Demandes</title>
-                    <link rel="stylesheet" href="/css/styleProfesseur.css">
+                    <link rel="stylesheet" href="styleProfesseur.css">
                 </head>
-            
+                
                 <body>
-                    <div class="back">
-                        <div class="mainSection">
-                            <h1>Demandes</h1>
+                    <div class="mainSection">
+                        <h1>Liste des demandes</h1>
+                
+                        <TABLE BORDER=1> 
+                        <TR> 
+                        <TH> Formation </TH> 
+                        <TH> Prénom </TH> 
+                        <TH> Nom </TH> 
+                        <TH> Description </TH> 
+                        <TH> Etat </TH>
+                        <TH> Intervenant </TH> 
+                        <TH> Valider </TH>
+                        </TR> 
+                        <TR> 
+                        <TD> MM </TD> 
+                        <TD> Élise </TD> 
+                        <TD> Dard </TD> 
+                        <TD> Salut Pierre, ça va ? <3 </TD> 
+                        <TD> en cours </TD> 
+                        <td> 
+                            <select>
+                                <option value="volvo">Ça va bien et toi ?</option>
+                                <option value="saab">t\'es qui ?</option>
+                                <option value="opel">Je t\'aime</option>
+                                <option value="audi">epouse moi</option>
+                            </select>
+                        </td>
+                        <TH> <button>Envoyer</button> </TH>
+                        </TR> 
+                        </TABLE>
                       
-                        </div>
-            		</div>
-            	</body>
+                    </div>
+                </body>
                 </html>';
     }
 }
